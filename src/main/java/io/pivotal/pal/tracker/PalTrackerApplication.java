d777f21738f49a91ae7f0f2d46efa5996b3dd8f5
Anand Waishetti <anand.walishetti@cognizant.com>
2017-12-04 15:29:11 -0600

    Simple Spring boot app

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by aw169 on 12/4/17.
+ */
+
+@SpringBootApplication
+public class PalTrackerApplication {
+
+    public static void main(String [] args){
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+}
+
---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker;
+
+/**
+ * Created by aw169 on 12/4/17.
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello PAL !";
+    }
+}
