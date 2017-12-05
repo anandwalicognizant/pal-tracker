59fd5d2495c3051355c81659a2dae879fd5846b2
Anand Waishetti <anand.walishetti@cognizant.com>
2017-12-05 12:54:30 -0600

    Added Concourse file

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -4,14 +4,23 @@ package io.pivotal.pal.tracker;
  * Created by aw169 on 12/4/17.
  */
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String welcomeMessage;
+
+    public WelcomeController(@Value("${welcome.message}")String message){
+        this.welcomeMessage = message;
+    }
+
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+
+        return this.welcomeMessage;
     }
 }
