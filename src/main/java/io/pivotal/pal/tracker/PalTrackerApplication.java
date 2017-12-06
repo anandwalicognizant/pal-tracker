eedee68a180279afabc6bcee3ba84de586cba072
Anand Waishetti <anand.walishetti@cognizant.com>
2017-12-06 13:09:57 -0600

    Updated for jdbc template lab

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import javax.sql.DataSource;
 
 /**
  * Created by aw169 on 12/4/17.
@@ -16,13 +19,17 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    @Autowired
+    private DataSource dataSource;
+
     public static void main(String [] args){
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
     @Bean
     TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     @Bean
